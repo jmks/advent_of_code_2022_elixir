35198d4fad8e803f0737f5b42350ea29df8a84be
Jason Schweier <jason.schweier@gmail.com>
2022-12-04 09:10:27 -0500

    Day 3, part 2

--- test/advent_of_code2022_elixir/day_03_rucksack_reogranization_test.exs
+++ test/advent_of_code2022_elixir/day_03_rucksack_reogranization_test.exs
@@ -16,4 +16,9 @@ defmodule AdventOfCode2022Elixir.Day03RucksackReogranizationTest do
     assert sum_of_priorities(@example) == 157
     assert sum_of_priorities(Input.strings(3)) == 7997
   end
+
+  test "sum_of_badge_priorities" do
+    assert sum_of_badge_priorities(@example) == 70
+    assert sum_of_badge_priorities(Input.strings(3)) == 2545
+  end
 end
