7d66757e19e55c0ee196038079a95fbee450c970
Jason Schweier <jason.schweier@gmail.com>
2022-12-04 09:55:38 -0500

    Day 4, part 2

--- lib/advent_of_code2022_elixir/day_04_camp_cleanup.ex
+++ lib/advent_of_code2022_elixir/day_04_camp_cleanup.ex
@@ -44,7 +44,21 @@ defmodule AdventOfCode2022Elixir.Day04CampCleanup do
   Some of the pairs have noticed that one of their assignments fully contains the other. For example, 2-8 fully contains 3-7, and 6-6 is fully contained by 4-6. In pairs where one assignment fully contains the other, one Elf in the pair would be exclusively cleaning sections their partner will already be cleaning, so these seem like the most in need of reconsideration. In this example, there are 2 such pairs.
 
   In how many assignment pairs does one range fully contain the other?
-  s
+
+  --- Part Two ---
+
+  It seems like there is still quite a bit of duplicate work planned. Instead, the Elves would like to know the number of pairs that overlap at all.
+
+  In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and 2-6,4-8) do overlap:
+
+  5-7,7-9 overlaps in a single section, 7.
+  2-8,3-7 overlaps all of the sections 3 through 7.
+  6-6,4-6 overlaps in a single section, 6.
+  2-6,4-8 overlaps in sections 4, 5, and 6.
+
+  So, in this example, the number of overlapping assignment pairs is 4.
+
+  In how many assignment pairs do the ranges overlap?
   """
   def parse(input) do
     input
@@ -55,16 +69,25 @@ defmodule AdventOfCode2022Elixir.Day04CampCleanup do
     end)
   end
 
-  def count_entirely_already_assigned(assignments) do
-    assignments
-    |> Enum.count(fn [{x1, x2}, {y1, y2}] ->
+  def count_entirely_already_assigned(input) do
+    input
+    |> parse()
+    |> Enum.count(fn [x1..x2, y1..y2] ->
       (y1 <= x1 and x2 <= y2) or (x1 <= y1 and y2 <= x2)
+    end)
+  end
+
+  def count_overlapping(input) do
+    input
+    |> parse()
+    |> Enum.count(fn [x, y] ->
+      not Range.disjoint?(x, y)
     end)
   end
 
   defp parse_range(range) do
     [lower, upper] = String.split(range, "-", parts: 2, trim: true)
 
-    {String.to_integer(lower), String.to_integer(upper)}
+    String.to_integer(lower)..String.to_integer(upper)
   end
 end
--- test/advent_of_code2022_elixir/day_04_camp_cleanup_test.exs
+++ test/advent_of_code2022_elixir/day_04_camp_cleanup_test.exs
@@ -14,17 +14,22 @@ defmodule AdventOfCode2022Elixir.Day04CampCleanupTest do
 
   test "parses pair assigments" do
     assert parse(@example) == [
-      [{2, 4}, {6, 8}],
-      [{2, 3}, {4, 5}],
-      [{5, 7}, {7, 9}],
-      [{2, 8}, {3, 7}],
-      [{6, 6}, {4, 6}],
-      [{2, 6}, {4, 8}],
+      [2..4, 6..8],
+      [2..3, 4..5],
+      [5..7, 7..9],
+      [2..8, 3..7],
+      [6..6, 4..6],
+      [2..6, 4..8],
     ]
   end
 
   test "count_entirely_already_assigned/1" do
-    assert count_entirely_already_assigned(parse(@example)) == 2
-    assert count_entirely_already_assigned(parse(Input.strings(4))) == 524
+    assert count_entirely_already_assigned(@example) == 2
+    assert count_entirely_already_assigned(Input.strings(4)) == 524
+  end
+
+  test "count_overlapping/1" do
+    assert count_overlapping(@example) == 4
+    assert count_overlapping(Input.strings(4)) == 798
   end
 end
